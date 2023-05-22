936f2ee9bb23651d674f15adc1b9e090fe0af782
Miguel Palomares <mmap187@gmail.com>
2023-05-22 15:24:38 -0300

    add js for search

--- scripts/search.js
+++ scripts/search.js
@@ -3,28 +3,32 @@
 //     console.log (producto);
 //     return producto === Teclado;
 // });
-// console.log('Found', found);
-// document.getElementById("inputSearch").addEventListener("keyup", searchProduct);
-// function searchProduct(){
-//     filter = inputSearch.value.toLowerCase();
-//     li = box_search.getElementsByTagName("li");
-//     //Recorrer los li
-//     for ( i = 0; i < li.length; i++){
-//         products = li[i].getElementsByTagName("products")[0];
-//         textValue = a.textContent || products.innerText;
+// // console.log('Found', found);
+// // document.getElementById("inputSearch").addEventListener("keyup", searchProduct);
+// // function searchProduct(){
+// //     filter = inputSearch.value.toLowerCase();
+// //     li = box_search.getElementsByTagName("li");
+// //     //Recorrer los li
+// //     for ( i = 0; i < li.length; i++){
+// //         products = li[i].getElementsByTagName("products")[0];
+// //         textValue = a.textContent || products.innerText;
 
-//         if(textValue.toLowerCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         }
-//         else{
-//             li[i].style.display = "none";
-//         }
-//     }
-//     };
-document.addEventListener("keyup", e => {
-    if (e.target.matches("#buscador")) {
+// //         if(textValue.toLowerCase().indexOf(filter) > -1) {
+// //             li[i].style.display = "";
+// //         }
+// //         else{
+// //             li[i].style.display = "none";
+// //         }
+// //     }
+// //     };
+document.addEventListener("keydown", e => {
+    /*Ajustar el buscador*/
+    if (!e.target.matches("#buscador")) {/*
         document.querySelectorAll('.products').forEach(products => {
             console.log(products);
-        })
+        })*/
+        const nameProduct = document.getElementById('search').value;
+        /*const productSearch = products.find(products.title === nameProduct);*/
+        console.log(nameProduct)
     }
 })
